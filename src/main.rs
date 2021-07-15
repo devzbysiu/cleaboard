6e9d8c5a22bced1f19fb3a4de93d710534909545
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2021-07-15 21:21:06 +0200

    feature: Add dummy druid app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use log::debug;
+use druid::widget::{Button, Flex, Label};
+use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
 
-fn main() {
-    pretty_env_logger::init();
-    debug!("Starting");
-    println!("Hello, world!");
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(ui_builder());
+    let data = 0_u32;
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(data)
+}
+
+fn ui_builder() -> impl Widget<u32> {
+    let text =
+        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
+    let label = Label::new(text).padding(5.0).center();
+    let button = Button::new("increment")
+        .on_click(|_ctx, data, _env| *data += 1)
+        .padding(5.0);
+
+    Flex::column().with_child(label).with_child(button)
 }
