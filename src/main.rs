25750fc180245cd89e40493d2bbbed3d48359399
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2021-07-16 20:45:14 +0200

    feature: Add initial version of ui

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
 use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::{AppLauncher, Data, Env, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
 
-fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder());
-    let data = 0_u32;
-    AppLauncher::with_window(main_window)
-        .log_to_console()
-        .launch(data)
+const TURN_OFF_LABEL: &str = "Turn off the keyboard";
+const TURN_ON_LABEL: &str = "Turn on the keyboard";
+
+const TURNED_OFF_HEADER: &str = "Keyboard is turned off! You can start cleaning :)";
+const TURNED_ON_HEADER: &str = "Keyboard is turned on";
+
+#[derive(Clone, Data, Lens)]
+struct State {
+    enabled: bool,
 }
 
-fn ui_builder() -> impl Widget<u32> {
-    let text =
-        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
-    let label = Label::new(text).padding(5.0).center();
-    let button = Button::new("increment")
-        .on_click(|_ctx, data, _env| *data += 1)
-        .padding(5.0);
+impl State {
+    fn new(enabled: bool) -> Self {
+        Self { enabled }
+    }
+}
 
-    Flex::column().with_child(label).with_child(button)
+fn get_button_text(enabled: bool) -> String {
+    if enabled {
+        TURN_OFF_LABEL.into()
+    } else {
+        TURN_ON_LABEL.into()
+    }
+}
+
+fn get_header_text(enabled: bool) -> String {
+    if enabled {
+        TURNED_ON_HEADER.into()
+    } else {
+        TURNED_OFF_HEADER.into()
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::new(true)
+    }
+}
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(ui_builder())
+        .window_size((400., 70.))
+        .set_position((800., 200.));
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(State::default())
+}
+
+fn ui_builder() -> impl Widget<State> {
+    let header = Label::new(|state: &State, _env: &Env| get_header_text(state.enabled))
+        .padding(5.0)
+        .center();
+
+    let button = Button::new(|state: &State, _env: &Env| get_button_text(state.enabled))
+        .on_click(|_ctx, state, _env| state.enabled = !state.enabled)
+        .padding(15.0);
+
+    Flex::column().with_child(header).with_child(button)
 }
