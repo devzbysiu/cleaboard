b3be77287120768e57dd270d0b1737ce164a6e1f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-24 19:04:52 +0100

    refactor: Fix formatting

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -29,7 +29,7 @@ impl Keyboard for PcKeyboard {
 
 pub(crate) fn check_xinput() -> Result<()> {
     match Command::new("xinput").arg("--version").status() {
-            Ok(status) if status.success() => Ok(()),
-            _ => Err(anyhow!("Looks like 'xinput' is not available. Please ensure it's working by running 'xinput --version'"))
-        }
+        Ok(status) if status.success() => Ok(()),
+        _ => Err(anyhow!("Looks like 'xinput' is not available. Please ensure it's working by running 'xinput --version'")),
+    }
 }
